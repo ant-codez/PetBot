2a872ab20ca44294b4e90385380cd2985f305f84
ant-codez <achavez@student.42.us.org>
2021-04-21 01:26:09 -0700

    added more animals to attributes page. Added stats lookup on PETS database with Emoji print out. added few tweeks to db fucntions as well

--- pet/abstract_pet_factory.py
+++ pet/abstract_pet_factory.py
@@ -9,5 +9,5 @@ class AbstractPetFactory:
     # creates and returns a pet with randomly generated attributes
     def create_pet(self, name):
         self.pet = AbstractPet(name, random.choice(list(Species)), random.choice(list(Size)), random.choice(list(AbilityType)), random.choice(list(AbilityType)), random.choice(list(Color)), random.choice(list(Rarity)))
-        print("-Pet Created: ", self.pet.name, random.choice(list(Species)), random.choice(list(Size)), random.choice(list(AbilityType)), random.choice(list(AbilityType)), random.choice(list(Color)), random.choice(list(Rarity)))
+        print("-Pet Created: ", self.pet.name, self.pet.species, self.pet.size, self.pet.ability_type, self.pet.subtype, self.pet.color, self.pet.rarity)
         return self.pet
